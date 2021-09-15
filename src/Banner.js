429cf8cea4cd7a9899b8e19c5391399e20e9cebe
shivamvora <shivampatel27102001@gmail.com>
2021-09-16 00:40:00 +0530

    changes

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
 import './Banner.css';
+import Search from './Search';
 
 const Banner = () => {
+    const [showSearch, setShowSearch] = useState( false );
     return (
         <div className="banner">
-            <div>
-                
+            <div className="banner__search">
+                {showSearch && <Search />}
+                <Button variant="outlined" className="banner__searchButton" onClick={() => setShowSearch( !showSearch )}>Search Dates</Button>
             </div>
-        </div> 
+            <div className="banner_info">
+                <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of getway to uncover the hidden gems near you</h5>
+                <Button variant="outlined">Explore Nearby</Button>
+            </div>
+        </div>
     )
 }
 
---
+++ src/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Search.css';
+const Search = () => {
+    return (
+        <div className="search">
+
+        </div>
+    )
+}
+
+export default Search
